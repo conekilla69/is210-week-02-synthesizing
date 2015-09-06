8305a3712b5e030740a2cb3b3f55099ab9d7f470
conekilla69 <wrcfan@verizon.net>
2015-09-06 16:56:50 +0000

    ran 2 tests after changes were done,passed runtests & pylint.

--- task_02.py
+++ task_02.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+"""Task for assignment, this is my docstring."""
 print "Hello Student!"
--- task_03.py
+++ task_03.py
@@ -1,7 +1,7 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This is a docstring for the peanut gallery"""
+CHARLIE = "Chocolate Factory"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
